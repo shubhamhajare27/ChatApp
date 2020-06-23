fe5b241b236a21dfa78c322801830ee485a8668e
Shubham Hajare <shubhamhajare4@gmail.com>
2020-06-23 20:21:44 +0530

    Signalr

---
+++ ChatApp/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void send(string user,string message)
+        {
+            Clients.All.recieveMessage(user,message);
+        }
+    }
+}
---
+++ ChatApp/Scripts/Signalr.js
@@ -0,0 +1,20 @@
+﻿(function () { //IIFE (Immediate Invoked Function Expression)
+
+    var chat = $.connection.chatHub; // connection to ChatHub variable created
+    $.connection.hub.start()         // connection Started
+        .done(function () {          // if success
+
+            $("#buttonid").click(function () {
+                var textbox1 = $("#userid").val();     // get username
+                var textbox2 = $("#msgid").val();      // get message
+                chat.server.send(textbox1, textbox2);  // call send method on the hub and send username and message as parameters
+            });
+            
+    }).fail(function () { console.log("Failed!!!"); }); // if failed 
+
+    chat.client.recieveMessage = function (username, message) {  // client receives the username and message 
+
+        $("#chat-message").append(username + " says " + message + "<br />");
+    }
+
+})()
